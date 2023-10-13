5cb905bd8f45dcc94de0a1880e8713173d969eab
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-14 02:28:57 +0600

    added add coffee form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,7 +1,35 @@
 const AddCoffee = () => {
   return (
-    <div>
-      <h2 className="text-4xl font-bold">Add Coffee</h2>
+    <div className="bg-[#F4F3F0] max-w-5xl mx-auto text-center mt-20 p-4">
+      <h2 className="text-4xl font-bold">Add New Coffee</h2>
+      <form>
+        <div className="flex justify-around">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Enter coffee name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                placeholder="Available quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
